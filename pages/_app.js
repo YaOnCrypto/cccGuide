d2ab2f9540652f6a20f92e0b98166f9fbf07d0e3
ysf-ad <yousifaldakoki@gmail.com>
2022-03-07 21:46:04 -0500

    v2.2

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,7 @@ import { ParallaxProvider } from "react-scroll-parallax";
 export default function MyApp({ Component, pageProps }) {
   return (
     <ParallaxProvider>
+      <link href='https://db.onlinewebfonts.com/c/afa327cd559b36de171501fc06a854f7?family=8-bit+HUD' rel='stylesheet' type='text/css'></link>
       <Component {...pageProps} />
     </ParallaxProvider>
   );
